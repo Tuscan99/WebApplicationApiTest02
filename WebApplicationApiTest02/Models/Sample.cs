4a74cf43603891c016fa6962f1043a7fb605e714
Studio <studio.2022@yandex.ru>
2023-04-23 17:14:00 +0600

    deleting test data

--- WebApplicationApiTest02/Models/Sample.cs
+++ WebApplicationApiTest02/Models/Sample.cs
@@ -13,15 +13,6 @@ namespace WebApplicationApiTest02.Models
                 context.Database.Migrate();
             }
 
-            if (!context.r_currency.Any())
-            {
-                DateOnly testdate = new DateOnly(2001, 01, 01);
-                context.r_currency.AddRange(
-                    new Currency { Title = "Test1", Code = "CR1", Value = 20.012M, A_Date = testdate }
-                );
-
-                context.SaveChanges();
-            }
         }
     }
 }
